69f4145ad277985bd764f5e39416d6d238d7dfcb
Rohit Saha <r.saha1997@gmail.com>
2020-03-10 14:37:59 -0400

    modified losses

--- models/utils/losses.py
+++ models/utils/losses.py
@@ -33,10 +33,11 @@ def ridge_weight_decay(parameters):
     return weight_decay
 
 
-def ssim_loss(prediction, ground_truth, max_val,
+def ssim_loss(prediction, ground_truth, max_val=2.,
                 filter_size=11, filter_sigma=1.5,
                 k1=0.01, k2=0.03):
 
+    # Try block filter of 8x8
     # Gaussian filter of size 11x11 and width 1.5
     # is used. Image has to be at least 11x11 big.
 
