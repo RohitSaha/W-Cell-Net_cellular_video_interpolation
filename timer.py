4319f81ed4960a8e9d96c4a588dde0281ca8022d
Rohit Saha <r.saha1997@gmail.com>
2020-03-24 02:45:59 -0400

    script to introduce wait times between running 2 models. This ensures that the GPU has enough time to release resources for one program before starting the next program

---
+++ timer.py
@@ -0,0 +1,12 @@
+import time
+
+wait_time = 30
+
+start = time.time()
+diff = time.time() - start
+
+print('Waiting time begins.....')
+while(diff < wait_time):
+    diff = time.time() - start
+
+print('Starting new program.....')
