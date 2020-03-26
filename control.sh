96b9a5d7c8d79b3867c5b6b936e8590c24c9f877
Rohit Saha <r.saha1997@gmail.com>
2020-03-26 01:29:54 -0400

    final bash control script to run several experiments

--- control.sh
+++ control.sh
@@ -4,12 +4,25 @@
 
 limit_IF=7
 limit_out_channels=32
+exp_name='slack_20px_fluorescent_window_'
+
+for (( n_IF=6; n_IF<=$limit_IF; n_IF++ ))
+    do
+        for (( out_channels=8; out_channels<=$limit_out_channels; out_channels=out_channels*2 ))
+            do
+                bash run_skip_conn_separate_encoder_bipn.sh $out_channels $n_IF $exp_name$(($n_IF+2)) 
+                echo "Program OC: $out_channels IF: $n_IF complete"
+                python timer.py
+            done
+    done
+
+echo 'skip-conn experiments complete'
 
 for (( n_IF=4; n_IF<=$limit_IF; n_IF++ ))
     do
         for (( out_channels=8; out_channels<=$limit_out_channels; out_channels=out_channels*2 ))
             do
-                bash run_skip_conn_separate_encoder_bipn.sh $out_channels $n_IF
+                bash run_unet_separate_encoder_bipn.sh $out_channels $n_IF $exp_name$(($n_IF+2))
                 echo "Program OC: $out_channels IF: $n_IF complete"
                 python timer.py
             done
