ce47099cf80de92de47d5fb2a1f1aa90abacc774
Rohit Saha <r.saha1997@gmail.com>
2020-04-20 19:15:27 -0400

    changing model names to make it less eccentric

--- control.sh
+++ control.sh
@@ -17,12 +17,13 @@ for (( n_IF=6; n_IF<=$limit_IF; n_IF++ ))
     done
 
 echo 'skip-conn experiments complete'
+echo 'Beginning w-net experiments'
 
 for (( n_IF=4; n_IF<=$limit_IF; n_IF++ ))
     do
         for (( out_channels=8; out_channels<=$limit_out_channels; out_channels=out_channels*2 ))
             do
-                bash run_unet_separate_encoder_bipn.sh $out_channels $n_IF $exp_name$(($n_IF+2))
+                bash run_wnet.sh $out_channels $n_IF $exp_name$(($n_IF+2))
                 echo "Program OC: $out_channels IF: $n_IF complete"
                 python timer.py
             done
